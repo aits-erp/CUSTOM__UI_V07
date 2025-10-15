1c46f2dda5f8c0e3a8c862b37eddb264159c0fef
uNik020 <shekharnikhil020@gmail.com>
2025-10-15 06:34:52 +0000

    dashboard path changes

--- custom_ui_v7/public/dist/js/nova_ui.bundle.YZLWGP6W.js
+++ custom_ui_v7/public/dist/js/nova_ui.bundle.YZLWGP6W.js
@@ -249,7 +249,7 @@
       rightBtn.style.border = "none";
       rightBtn.style.borderRadius = "50%";
       rightBtn.style.cursor = "pointer";
-      rightBtn.style.backgroundColor = "#ffffff";
+      rightBtn.style.backgroundColor = "#090202ff";
       rightBtn.style.boxShadow = "0 4px 10px rgba(0,0,0,0.2)";
       rightBtn.style.zIndex = "9999";
       rightBtn.onclick = () => {
@@ -497,13 +497,13 @@
           .c-teal { background: #16a085; }
         </style>
         <div class="nova-grid" id="main-grid" nova-animate-in>
-          <div class="nova-card" onclick="frappe.set_route('app/dashboard-view/Total%20Outstanding%20Amount%20Of%20Distributors%20Group%2060')">
+          <div class="nova-card" onclick="frappe.set_route('app/dashboard-view/Outstanding%20Of%20Distributor%20Over%2060%20Days')">
         <div class="nova-icon c-yellow"><img src="/assets/custom_ui/images/D2.png" style="width:60px; height:50px; object-fit: contain;" /></div><div>Total Outstanding Amount Of Distributors Group 60</div>
     </div>
     <div class="nova-card" onclick="frappe.set_route('app/dashboard-view/Product%20Ageing%20over%20180%20Days')">
         <div class="nova-icon c-orange"><img src="/assets/custom_ui/images/time.png" style="width:60px; height:50px; object-fit: contain;" /></div><div>Product Ageing over 180 Days</div>
     </div>
-    <div class="nova-card" onclick="frappe.set_route('app/dashboard/Outstanding%20Debtors%20Monthwise')">
+    <div class="nova-card" onclick="frappe.set_route('app/dashboard-view/Outstanding%20Of%20Debtors%20Monthwise')">
         <div class="nova-icon c-blue"><img src="/assets/custom_ui/images/debt.png" style="width:60px; height:50px; object-fit: contain;" /></div><div>Outstanding Debtors Monthwise</div>
     </div>
     <div class="nova-card" onclick="frappe.set_route('app/dashboard-view/Top%2050%20selling%20items')">
@@ -597,4 +597,4 @@
     createMiniNav();
   })();
 })();
-//# sourceMappingURL=nova_ui.bundle.KSRYWRRL.js.map
+//# sourceMappingURL=nova_ui.bundle.YZLWGP6W.js.map
